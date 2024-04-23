e0019e95360a0600aa5d9a8330fff2943474c02b
gauravghodinde <gauravghodinde@gmail.com>
2024-04-24 05:23:14 +0530

    added ese backend

---
+++ src/main/java/com/ese/spring/login/SpringBootESEApplication.java
@@ -0,0 +1,13 @@
+package com.ese.spring.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootESEApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootESEApplication.class, args);
+	}
+
+}
